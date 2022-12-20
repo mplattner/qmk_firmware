0a16bd24c83be9a15ffd4485ef5b1eb5961b4bee
Martin Plattner <git@mplattner.at>
2022-12-20 03:44:48 +0100

    update keymap

--- keyboards/sculpter/keymaps/mplattner/config.h
+++ keyboards/sculpter/keymaps/mplattner/config.h
@@ -1,10 +1,11 @@
 #define ONESHOT_TAP_TOGGLE 20
 #define ONESHOT_TIMEOUT 1000
 
+#define AUTO_SHIFT_MODIFIERS
 #define AUTO_SHIFT_TIMEOUT 150
 
 // number of times TT(layer) needs to be tapped to toggle layer
-#define TAPPING_TOGGLE 2
+#define TAPPING_TOGGLE 10
 
 // tap dance duration
 //#define TAPPING_TERM 250
