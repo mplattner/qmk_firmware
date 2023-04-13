4eec608c552a98ec79b8b60a8a92c2b45dee51fb
Martin Plattner <git@mplattner.at>
2023-04-14 01:16:04 +0200

    try to fix AUTO_SHIFT_MODIFIERS issue

--- keyboards/sculpter/keymaps/mplattner/config.h
+++ keyboards/sculpter/keymaps/mplattner/config.h
@@ -1,14 +1,16 @@
-#define ONESHOT_TAP_TOGGLE 20
+#define ONESHOT_TAP_TOGGLE 0
 #define ONESHOT_TIMEOUT 1000
 
 #define AUTO_SHIFT_MODIFIERS
-#define AUTO_SHIFT_TIMEOUT 150
+#define AUTO_SHIFT_TIMEOUT 142
+
+//#define DOUBLE_REPORT
 
 // number of times TT(layer) needs to be tapped to toggle layer
-#define TAPPING_TOGGLE 10
+// #define TAPPING_TOGGLE 10
 
 // tap dance duration
 //#define TAPPING_TERM 250
 
 // trying to fix issue that occurs since enabling AUTO_SHIFT_MODIFIERS, see https://github.com/qmk/qmk_firmware/issues/19671
-#define TAP_CODE_DELAY 10
+#define TAP_CODE_DELAY 20
