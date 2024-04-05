c738ce03f4bbb8976be11357fdd7939242f091ab
Martin Plattner <git@mplattner.at>
2024-04-05 17:54:57 +0200

    add coding layer

--- keyboards/sculpter/keymaps/mplattner/keymap.c
+++ keyboards/sculpter/keymaps/mplattner/keymap.c
@@ -15,11 +15,15 @@
  */
 #include QMK_KEYBOARD_H
 
+#define TH(key, hold_key) register_code16((!shifted) ? key : hold_key); return;
+#define THU(key, hold_key) unregister_code16((!shifted) ? key : hold_key); return;
+
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
     _ALTGR,
-    _NUM
+    _NUM,
+    _CODE
 };
 
 enum custom_keycodes {
@@ -39,15 +43,15 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-      KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS, QK_BOOT,
+      KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS, QK_RBT,
       KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL, KC_HOME,
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_NUBS, KC_END,
       OSM(MOD_LCTL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT, C(KC_V), KC_PGUP,
       OSM(MOD_LSFT), KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT), KC_UP, KC_PGDN,
-      OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), LT(_NUM, KC_SPC), LT(_ALTGR, KC_SPC), OSL(_NUM), TG(_ALTGR), A(KC_SPC), KC_LEFT, KC_DOWN, KC_RGHT
+      OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), LT(_NUM, KC_SPC), LT(_ALTGR, KC_SPC), TG(_CODE), TG(_ALTGR), A(KC_SPC), KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_ALTGR] = LAYOUT(
-      KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_MPLY, KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR, KC_SCRL, KC_MPLY, QK_RBT,
+      KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_MPLY, KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR, KC_SCRL, KC_MPLY, QK_BOOT,
       KC_GRV, KC_1, KC_2, KC_NUHS, ALGR(KC_4), KC_5, KC_6, KC_7, KC_8, KC_9, KC_MPRV, KC_MNXT, KC_EQL, KC_BSPC, KC_DEL, KC_HOME,
       KC_TAB, A(KC_F4), KC_ESC, KC_ENT, KC_BSPC, KC_TAB, C(KC_Y), ALGR(KC_U), KC_UP, ALGR(KC_O), P_PWD, KC_LBRC, KC_RBRC, KC_NUBS, KC_END,
       OSM(MOD_LCTL), ALGR(KC_A), ALGR(KC_S), KC_DEL, C(S(KC_SPC)), KC_TAB, C(KC_Z), KC_LEFT, KC_DOWN, KC_RIGHT, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT, KC_INS, KC_PGUP,
@@ -62,6 +66,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       OSM(MOD_LSFT), KC_NUBS, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_B, KC_EQL, KC_PLUS, KC_LCBR, KC_RCBR, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN,
       OSM(MOD_LCTL), KC_LGUI, KC_LALT, KC_ENT, KC_TRNS, OSL(_NUM), KC_TRNS, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
+    [_CODE] = LAYOUT(
+      KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS, QK_RBT,
+      KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL, KC_HOME,
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_NUBS, KC_END,
+      OSM(MOD_LCTL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT, C(KC_V), KC_PGUP,
+      OSM(MOD_RSFT), KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT), KC_UP, KC_PGDN,
+      OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), LT(_NUM, KC_SPC), MT(MOD_RSFT, KC_SPC), TG(_CODE), TG(_ALTGR), A(KC_SPC), KC_LEFT, KC_DOWN, KC_RGHT
+    ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -70,4 +82,105 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     }
     return true;
+}
+
+void autoshift_press_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
+    bool l_shift_held = get_mods() & MOD_BIT(KC_LSFT);
+    bool l_shift_held_osm = get_oneshot_mods() & MOD_BIT(KC_LSFT);
+    bool r_shift_held = get_mods() & MOD_BIT(KC_RSFT);
+    bool r_shift_held_osm = get_oneshot_mods() & MOD_BIT(KC_RSFT);
+
+    bool shift_held = l_shift_held || r_shift_held || l_shift_held_osm || r_shift_held_osm;
+
+    if (IS_LAYER_ON(_CODE) && !shift_held) {
+        //set_oneshot_mods(get_oneshot_mods() & (~MOD_BIT(KC_LSFT)))
+
+        switch(keycode) {
+            case KC_Q: TH(KC_Q, C(KC_W));
+            case KC_W: TH(KC_W, KC_ESC);
+            case KC_E: TH(KC_E, KC_ENT);
+            case KC_R: TH(KC_R, KC_BSPC);
+            case KC_T: TH(KC_T, KC_TAB);
+
+            case KC_Y: TH(KC_Y, C(KC_Y));
+
+            case KC_D: TH(KC_D, KC_DEL);
+            case KC_G: TH(KC_G, KC_GRV);
+
+            case KC_Z: TH(KC_Z, C(KC_Z));
+            case KC_X: TH(KC_X, C(KC_X));
+            case KC_C: TH(KC_C, C(KC_C));
+            case KC_V: TH(KC_V, C(KC_V));
+
+            // arroy keys
+            case KC_I: TH(KC_I, KC_UP);
+            case KC_J: TH(KC_J, KC_LEFT);
+            case KC_K: TH(KC_K, KC_DOWN);
+            case KC_L: TH(KC_L, KC_RGHT);
+
+            case KC_H: TH(KC_H, KC_MINS);
+
+            case KC_N: TH(KC_N, KC_HOME);
+            case KC_M: TH(KC_M, KC_END);
+            case KC_COMM: TH(KC_COMM, KC_PGUP);
+            case KC_DOT: TH(KC_DOT, KC_PGDN);
+
+            case KC_SPC: TH(KC_SPC, KC_RSFT);
+        }
+    }
+
+    if (shifted) {
+        add_weak_mods(MOD_BIT(KC_LSFT));
+    }
+    // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
+    register_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
+}
+
+void autoshift_release_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
+    bool l_shift_held = get_mods() & MOD_BIT(KC_LSFT);
+    bool l_shift_held_osm = get_oneshot_mods() & MOD_BIT(KC_LSFT);
+    bool r_shift_held = get_mods() & MOD_BIT(KC_RSFT);
+    bool r_shift_held_osm = get_oneshot_mods() & MOD_BIT(KC_RSFT);
+
+    bool shift_held = l_shift_held || r_shift_held || l_shift_held_osm || r_shift_held_osm;
+
+    if (IS_LAYER_ON(_CODE) && !shift_held) {
+        switch(keycode) {
+            case KC_Q: THU(KC_Q, C(KC_W));
+            case KC_W: THU(KC_W, KC_ESC);
+            case KC_E: THU(KC_E, KC_ENT);
+            case KC_R: THU(KC_R, KC_BSPC);
+            case KC_T: THU(KC_T, KC_TAB);
+
+            case KC_Y: THU(KC_Y, C(KC_Y));
+
+            case KC_D: THU(KC_D, KC_DEL);
+            case KC_G: THU(KC_G, KC_GRV);
+
+            case KC_Z: THU(KC_Z, C(KC_Z));
+            case KC_X: THU(KC_X, C(KC_X));
+            case KC_C: THU(KC_C, C(KC_C));
+            case KC_V: THU(KC_V, C(KC_V));
+
+            // arroy keys
+            case KC_I: THU(KC_I, KC_UP);
+            case KC_J: THU(KC_J, KC_LEFT);
+            case KC_K: THU(KC_K, KC_DOWN);
+            case KC_L: THU(KC_L, KC_RGHT);
+
+            case KC_H: THU(KC_H, KC_MINS);
+
+            case KC_N: THU(KC_N, KC_HOME);
+            case KC_M: THU(KC_M, KC_END);
+            case KC_COMM: THU(KC_COMM, KC_PGUP);
+            case KC_DOT: THU(KC_DOT, KC_PGDN);
+
+            case KC_SPC: THU(KC_SPC, KC_RSFT);
+        }
+    }
+
+    // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
+    // The IS_RETRO check isn't really necessary here, always using
+    // keycode & 0xFF would be fine.
+    unregister_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
 }
