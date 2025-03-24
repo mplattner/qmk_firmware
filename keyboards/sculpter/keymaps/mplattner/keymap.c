ab955f935e1b79eb3211ae6961115373045126e5
Martin Plattner <git@mplattner.at>
2025-03-24 01:56:00 +0100

    fix tap dance with auto shift with more than 4 TD definitions

--- keyboards/sculpter/keymaps/mplattner/keymap.c
+++ keyboards/sculpter/keymaps/mplattner/keymap.c
@@ -323,8 +323,11 @@ void autoshift_press_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
     if (shifted) {
         add_weak_mods(MOD_BIT(KC_LSFT));
     }
-    // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
-    register_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
+
+    if ( ! IS_QK_TAP_DANCE(keycode)) {
+        // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
+        register_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
+    }
 }
 
 void autoshift_release_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
@@ -378,7 +381,9 @@ void autoshift_release_user(uint16_t keycode, bool shifted, keyrecord_t *record)
     // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
     // The IS_RETRO check isn't really necessary here, always using
     // keycode & 0xFF would be fine.
-    unregister_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
+    if ( ! IS_QK_TAP_DANCE(keycode)) {
+        unregister_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
+    }
 }
 
 void keyboard_post_init_user(void) {
